6534580909c396fed3998d49388b5b519479f846
Jonathan Loescher <jonathan@jloescher.com>
2023-02-04 21:47:46 -0500

    Added app to installed apps Updated model for the decimal spaces for the price field Ran makemigrations

--- products/models.py
+++ products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     description = models.CharField(
         max_length=255
     )  # Pretty sure IRL this would be a TextField
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory_quantity = models.IntegerField()
